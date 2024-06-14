970fd264d3d3efea319cd9ca79d38e2b3c33ac6d
tumuhaisemichael <michaelberry047@gmail.com>
2024-06-14 08:46:45 +0000

    creating a new file

---
+++ database/app1.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+# Path to the JSON file
+JSON_FILE_PATH = 'database_file.json'
+
+@app.route('/', methods=['GET'])
+def get_database():
+    try:
+        with open(JSON_FILE_PATH, 'r') as f:
+            database_data = json.load(f)
+    except FileNotFoundError:
+        database_data = {'error': 'Database file not found'}
+    
+    return jsonify(database_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
